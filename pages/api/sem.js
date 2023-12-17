f3c590cd776256b03103ea19702a872f7dfc439c
MalavMori <92566896+MalavMori@users.noreply.github.com>
2023-12-17 21:20:31 +0530

    Update sem.js

--- pages/api/sem.js
+++ pages/api/sem.js
@@ -1,14 +1,48 @@
-import { promises as fs } from 'fs';
+const sem3data = {
+    
+    show: true,
+    data:[
+        {
+            title:"study material",
+            data:[
+                {
+                    title:"SLP U-1",
+                    src:"unit-1-1.pdf"
+                },
+                {
+                    title:"DSA U-1",
+                    src:"unit-1-2.pdf"
+                },
+                {
+                    title:"SLP U-1",
+                    src:"unit-1-1.pdf"
+                },
+                {
+                    title:"SLP U-1",
+                    src:"unit-1-1.pdf"
+                }
+                
+            ]
+        },
+        {
+            title:"IMP",
+            data:[
+                {
+                    title:"IMP questions RDBMS",
+                    src:"important-questions-rdbms.pdf"
+                }
+                
+            ]
+        }
+    ]
+
+}
 export default async function handler(req, res) {
     console.log(req.query.sem);
-     const file = await fs.readFile(process.cwd() + `/kjdb/${req.query.sem}.json`, 'utf8',(err,data)=>{
-        const jsondata = JSON.parse(data);
-        if(jsondata.show){
-            res.status(200).json(JSON.parse(data).data)
-        }else{
-            res.status(200).json({status:500})
-        }
-    });
-    
+    if (sem3data.show) {
+        res.send(200).json(sem3data.data)
+    }else{
+        res.send(200).json({data:false})
+    }
   }
   
