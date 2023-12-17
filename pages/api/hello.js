2bd25f6c1ebfd4be57181bf644f4fbbebbe7461a
MalavMori <92566896+MalavMori@users.noreply.github.com>
2023-12-17 19:55:14 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const fs = require("fs")
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //res.status(200).json({ name: 'John Doe' })
+  console.log(req.query.sem);
+    const file = fs.readFile(`kjdb/${req.query.sem}.json`,"utf8",(err,data)=>{
+        const jsondata = JSON.parse(data);
+        if(jsondata.show){
+            res.status(200).json(JSON.parse(data).data)
+        }else{
+            res.status(200).json({status:500})
+        }
+    })
 }
