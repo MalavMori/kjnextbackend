7838b1e20b04fba0c74d44cd87592ea0520793d3
MalavMori <92566896+MalavMori@users.noreply.github.com>
2023-12-17 20:55:55 +0530

    Update sem.js

--- pages/api/sem.js
+++ pages/api/sem.js
@@ -1,13 +1,14 @@
-import * as fs from "fs";
-export default function handler(req, res) {
+import { promises as fs } from 'fs';
+export default async function handler(req, res) {
     console.log(req.query.sem);
-    fs.readFile(`kjdb/${req.query.sem}.json`,"utf8",(err,data)=>{
+     const file = await fs.readFile(process.cwd() + `/kjdb/${req.query.sem}.json`, 'utf8',(err,data)=>{
         const jsondata = JSON.parse(data);
         if(jsondata.show){
             res.status(200).json(JSON.parse(data).data)
         }else{
             res.status(200).json({status:500})
         }
-    })
+    });
+    
   }
   
