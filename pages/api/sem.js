1acf9ace6ae70ace9603e9b29213554eec7bdf66
MalavMori <92566896+MalavMori@users.noreply.github.com>
2023-12-22 22:24:31 +0530

    Update sem.js

--- pages/api/sem.js
+++ pages/api/sem.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
         const sendreq = await fetch(`https://malavmori.github.io/jsonapi/kjdb/${req.query.data}.json`)
         const data = await sendreq.json()
         if (data.show) {
-            res.status(200).json(data)
+            res.status(200).json(data.data)
         }else{
             res.status(500).json({data:false})
         }
